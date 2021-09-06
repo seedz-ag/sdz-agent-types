94065dc2780bf0df40df682158696a26cf530ac5
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-09-06 14:07:59 -0300

    feat/pagination conector

--- interfaces/connector.interface.ts
+++ interfaces/connector.interface.ts
@@ -1,7 +1,11 @@
 import Client from "./client.interface";
 import Invoice from "./invoice.interface";
+import PaginationInterface from "./pagination.interface";
 
 export default interface Connector {
-  connect(): void
-  execute(query?: string): Promise<Client[] | Invoice[]>
+  connect(): void;
+  execute(
+    query?: string,
+    pagination?: PaginationInterface
+  ): Promise<Client[] | Invoice[]>;
 }
