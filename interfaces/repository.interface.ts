503fd54ee066145d8ec518733e03d21b269552bb
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-09-13 13:47:24 -0300

    reepository type

--- interfaces/repository.interface.ts
+++ interfaces/repository.interface.ts
@@ -2,22 +2,76 @@ import DatabaseRow from "./database-row.interface";
 import PaginationInterface from "./pagination.interface";
 
 export default interface Repository {
-  getClients(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getAddress(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getProperty(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getItem(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getItemBranding(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getItemGroup(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getRequest(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getRequestItem(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getInvoices(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getBilling(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getPaymentType(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getProvider(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getAccountPay(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getAccountReceivable(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getVendor(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getEmployee(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getInventory(pagination: PaginationInterface): Promise<DatabaseRow[]>;
-  getTest(pagination: PaginationInterface): Promise<DatabaseRow[]>;
+  getClients(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getAddress(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getProperty(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getItem(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getItemBranding(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getItemGroup(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getRequest(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getRequestItem(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getInvoices(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getBilling(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getPaymentType(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getProvider(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getAccountPay(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getAccountReceivable(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getVendor(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getEmployee(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getInventory(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
+  getTest(
+    pagination: PaginationInterface,
+    type: string
+  ): Promise<DatabaseRow[]>;
 }
