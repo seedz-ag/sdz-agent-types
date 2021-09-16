b12b30e6141ff42c150609e595b3e8a137a4d1bb
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-09-16 19:10:43 -0300

    count

--- interfaces/repository.interface.ts
+++ interfaces/repository.interface.ts
@@ -6,90 +6,144 @@ export default interface Repository {
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countClient(type: string): Promise<DatabaseRow[]>;
+  countClient(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getAddress(
     pagination: PaginationInterface,
     type?: string
   ): Promise<DatabaseRow[]>;
-  countAddress(type?: string): Promise<DatabaseRow[]>;
+  countAddress(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getProperty(
     pagination: PaginationInterface,
     type?: string
   ): Promise<DatabaseRow[]>;
-  countProperty(type?: string): Promise<DatabaseRow[]>;
+  countProperty(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getItem(
     pagination: PaginationInterface,
     type?: string
   ): Promise<DatabaseRow[]>;
-  countItem(type?: string): Promise<DatabaseRow[]>;
+  countItem(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getItemBranding(
     pagination: PaginationInterface,
     type?: string
   ): Promise<DatabaseRow[]>;
-  countItemBranding(type?: string): Promise<DatabaseRow[]>;
+  countItemBranding(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getItemGroup(
     pagination: PaginationInterface,
     type?: string
   ): Promise<DatabaseRow[]>;
-  countItemGroup(type?: string): Promise<DatabaseRow[]>;
+  countItemGroup(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getRequest(
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countRequest(type: string): Promise<DatabaseRow[]>;
+  countRequest(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getRequestItem(
     pagination: PaginationInterface,
     type?: string
   ): Promise<DatabaseRow[]>;
-  countRequestItem(type?: string): Promise<DatabaseRow[]>;
+  countRequestItem(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getInvoices(
     pagination: PaginationInterface,
     type?: string
   ): Promise<DatabaseRow[]>;
-  countInvoices(type?: string): Promise<DatabaseRow[]>;
+  countInvoices(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getInvoicesItem(
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countInvoicesItem(type: string): Promise<DatabaseRow[]>;
+  countInvoicesItem(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getPaymentType(
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countPaymentType(type?: string): Promise<DatabaseRow[]>;
+  countPaymentType(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getProvider(
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countProvider(type?: string): Promise<DatabaseRow[]>;
+  countProvider(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getAccountPay(
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countAccountPay(type: string): Promise<DatabaseRow[]>;
+  countAccountPay(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getAccountReceivable(
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countReceivable(type: string): Promise<DatabaseRow[]>;
+  countReceivable(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getVendor(
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countVendor(type?: string): Promise<DatabaseRow[]>;
+  countVendor(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getEmployee(
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countEmployee(type?: string): Promise<DatabaseRow[]>;
+  countEmployee(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getInventory(
     pagination: PaginationInterface,
     type: string
   ): Promise<DatabaseRow[]>;
-  countInventory(type?: string): Promise<DatabaseRow[]>;
+  countInventory(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
   getTest(
     pagination: PaginationInterface,
     type?: string
   ): Promise<DatabaseRow[]>;
-  countTest(type?: string): Promise<DatabaseRow[]>;
+  countTest(
+    pagination?: PaginationInterface,
+    type?: string
+  ): Promise<DatabaseRow[]>;
 }
