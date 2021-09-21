a2bc317fe510d90d7fb6e976196d5790da28e901
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-09-21 09:57:56 -0300

    scope type config

--- types/config.type.ts
+++ types/config.type.ts
@@ -2,17 +2,22 @@ import ConfigAuth from "./config.auth.type";
 import ConfigHttp from "./config.http.type";
 import ConfigDatabase from "../interfaces/config.database.interface";
 import ConfigSchedule from "./config.schedule.type";
+import ConfigScope from "./config.scope.type";
 
 export type { default as ConfigAuth } from "./config.auth.type";
 export type { default as ConfigFTP } from "./config.ftp.type";
 export type { default as ConfigDatabase } from "../interfaces/config.database.interface";
 export type { default as ConfigSchedule } from "./config.schedule.type";
+export type { default as ConfigScope } from "./config.scope.type";
 
 type Config = {
   auth: ConfigAuth;
   database: ConfigDatabase;
+  debug: boolean;
   http: ConfigHttp;
   schedule: ConfigSchedule;
-  debug: boolean;
+  pageSize: number;
+  scope: ConfigScope;
 };
+
 export default Config;
