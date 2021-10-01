5c80c70e844c66991dfe165583e12fb94c731208
Diogo <diogo.nunes>
2021-10-01 12:14:57 -0300

    ReadFile decorator updated

--- decorators/index.ts
+++ decorators/index.ts
@@ -1,2 +1,7 @@
+const ReadFileDecorator = require("./read-file");
 
-exports.ReadFile = require("./read-file");
+exports.ReadFile = ReadFileDecorator;
+
+module.exports = {
+  ReadFile: ReadFileDecorator,
+};
