675bd0ede24f53853def1f2ad2a2fb7a25d5e484
Diogo <diogo.nunes>
2021-10-01 12:08:31 -0300

    ReadFile decorator updated

--- decorators/read-file.ts
+++ decorators/read-file.ts
@@ -3,7 +3,7 @@ const { readFileSync } = require("fs");
 const ReadFile = (targetClass, targetKey, descriptor) => {
   const fn = descriptor.value;
   if (typeof fn === "function") {
-    descriptor.value = (file: string): string => {
+    descriptor.value = (file) => {
       const buffer = readFileSync(file);
       return JSON.parse(buffer.toString());
     };
