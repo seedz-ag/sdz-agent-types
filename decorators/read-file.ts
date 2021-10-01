de8e08958cb0a5b615a748fccb08f524a79da82b
Diogo <diogo.nunes>
2021-10-01 14:16:28 -0300

    ReadFile decorator updated

--- decorators/read-file.ts
+++ decorators/read-file.ts
@@ -1,12 +1,15 @@
 import { readFileSync } from "fs";
-const Decorator = (): any => (arg: any) =>
-  (arg.readFile = (file: string): string | undefined => {
-    let text;
-    try {
-      const buffer = readFileSync(file);
-      text = buffer.toString();
-    } catch {}
-    return text;
-  });
+const Decorator = (constructor: any) => {
+  return class extends constructor {
+    readFile(file: string): string | undefined {
+      let text;
+      try {
+        const buffer = readFileSync(file);
+        text = buffer.toString();
+      } catch {}
+      return text;
+    }
+  }
+};
 
 export default Decorator;
