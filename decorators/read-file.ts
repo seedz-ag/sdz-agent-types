52b49a264ca17039b5b22cbadc171409f7c1b10b
Diogo <diogo.nunes>
2021-10-01 12:07:39 -0300

    ReadFile decorator updated

--- decorators/read-file.ts
+++ decorators/read-file.ts
@@ -1,6 +1,6 @@
 const { readFileSync } = require("fs");
 
-const ReadFile = (targetClass, targetKey, descriptor): any => {
+const ReadFile = (targetClass, targetKey, descriptor) => {
   const fn = descriptor.value;
   if (typeof fn === "function") {
     descriptor.value = (file: string): string => {
