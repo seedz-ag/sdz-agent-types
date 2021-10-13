6b45c4c6b3da2922d904315ddb5d1e173ff576ef
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-10-13 10:12:52 -0300

    build

---
+++ dist/enums/erps.enum.d.ts
@@ -0,0 +1,4 @@
+export declare enum ERPs {
+    Linx = 0,
+    Protheus = 1
+}
--- dist/types/config.type.d.ts
+++ dist/types/config.type.d.ts
@@ -2,9 +2,11 @@ import ConfigAuthAPI from "./config.auth.api.type";
 import ConfigDatabase from "../interfaces/config.database.interface";
 import ConfigSchedule from "./config.schedule.type";
 import ConfigScope from "./config.scope.type";
+import { ERPs } from "../enums/erps.enum";
 import ConfigAuthFTP from "./config.auth.ftp.type";
 export type { default as ConfigAuthAPI } from "./config.auth.api.type";
 export type { default as ConfigAuthFTP } from "./config.auth.ftp.type";
+export type { default as ConfigDatabase } from "../interfaces/config.database.interface";
 export type { default as ConfigSchedule } from "./config.schedule.type";
 export type { default as ConfigScope } from "./config.scope.type";
 declare type Config = {
@@ -13,6 +15,7 @@ declare type Config = {
     connector: string;
     database: ConfigDatabase;
     debug: boolean;
+    erp: ERPs;
     ftp: ConfigAuthFTP;
     legacy: boolean;
     pageSize: number;
