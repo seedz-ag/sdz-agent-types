dab11c91d69d5a0aa94c9a879693740218edcb1c
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-10-15 10:38:53 -0300

    yarn lock

--- dist/interfaces/config.database.interface.d.ts
+++ dist/interfaces/config.database.interface.d.ts
@@ -2,10 +2,11 @@ import DatabaseDrivers from "../enums/database_drivers.enum";
 export default interface ConfigDatabaseInterface {
     driver: DatabaseDrivers;
     host: string;
-    locale: string;
-    schema: string;
+    locale?: string;
+    schema?: string;
     password: string;
     port: number;
-    server: string;
+    server?: string;
+    service?: string;
     username: string;
 }
