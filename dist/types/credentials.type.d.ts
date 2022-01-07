c63897b27393de7797f316cdfb01b6bc306afbf3
rubens.curvello <rubens.curvello@hotmail.com>
2022-01-07 17:08:52 -0300

    credentials client_secret

--- dist/types/credentials.type.d.ts
+++ dist/types/credentials.type.d.ts
@@ -1,6 +1,6 @@
 declare type Credentials = {
     client_id: string;
-    client_password: string;
+    client_secret: string;
     client_name?: string;
 };
 export default Credentials;
--- types/credentials.type.ts
+++ types/credentials.type.ts
@@ -1,6 +1,6 @@
 type Credentials = {
   client_id: string;
-  client_password: string;
+  client_secret: string;
   client_name?: string;
 };
 
